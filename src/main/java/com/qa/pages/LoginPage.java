2bd53253213dd6cfb2ab1587b6e6ad1a0a53d5dd
muleautomation <42306570+muleautomation@users.noreply.github.com>
2018-08-11 22:21:19 -0500

    add search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,7 +2,10 @@ package com.qa.pages;
 
 public class LoginPage {
 
-	public void LoginPage() {
+	public void loginPage() {
 		System.out.println("This is Login Page Method");
+	}
+	public void loginSet() {
+		System.out.println("This is Login Set Method");
 	}
 }
